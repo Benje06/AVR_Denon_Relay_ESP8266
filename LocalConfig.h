3a7fec2c2f908d912efa9d581edacfe7cf58e7ac
Jerome <benhaim_jerome@yahoo.fr>
2026-04-18 15:39:42 +0200

    CHR: stop tracking LocalConfig.h

--- LocalConfig.h
+++
@@ -1,9 +0,0 @@
-/*
- * Fichier de config spécifique à l'utilisateur
- */
-
-#define WLAN_SSID  "votre-ssid"
-#define WLAN_PASS  "votre-mot-de-passe"
-#define AVR_NAME   "AVRX-3000"     // Nom ou model de l'AVR
-#define AVR_IP     "192.168.x.x"   // IP de l'AVR sur le réseau
-#define AVR_PORT   80
